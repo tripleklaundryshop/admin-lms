47d7eca1ca0f617f640249a3c1440e6fc6af57cb
Rays30 <sabellanoraysel16@gmail.com>
2026-04-23 18:45:53 +0800

    Update firebase-config.js

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 // 2. Initialize Firebase Authentication
 export const auth = getAuth(app);
